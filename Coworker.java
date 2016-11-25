f7ec329b0706eaa0fbb461c07d0b1f32c3108c04
Alex <oleksandr.liakhovych@gmail.com>
2016-11-25 21:54:53 +0200

    initialCommit

---
+++ Coworker.java
@@ -0,0 +1,20 @@
+﻿Написать класс для отдела кадров – Сотрудник  (поля: фамилия, имя, отчество, дата рождения, пол, образования, номер документа об образовании, учебное заведение выдавшее документ, дата поступления на работу, домашний адрес).
+
+public class Coworker {
+
+        private String surname;
+	private String name;
+	private String secondName;
+	private String dateOfBirth;
+	private boolean sex;
+	private String education;
+	private long certificateId;
+	private String university
+	private String dateOfJobStart;
+	private String homeAddress;
+
+	// construsctor here
+	// getters here
+	// setters here
+
+}
